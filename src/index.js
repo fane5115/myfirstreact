f9cb686fb0c65956f5be1a1dbf34da88dd7b3432
fane5115 <fanef92@gmail.com>
2021-03-16 17:29:22 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const myfirstelement = <h1>merge</h1>
+const myfirstelement = <h1>mergedsada</h1>
 
 ReactDOM.render(myfirstelement, document.getElementById('root'));
