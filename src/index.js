634be958b16de862ff2648cc878ca794edb97e23
fane5115 <fanef92@gmail.com>
2021-03-16 17:31:01 +0200

    trimite

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const myfirstelement = <h1>mergedsada</h1>
+const myfirstelement = <h1>test merge</h1>
 
 ReactDOM.render(myfirstelement, document.getElementById('root'));
